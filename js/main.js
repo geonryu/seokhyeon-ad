749e212e3e25507b191d75767196968e758d8f0a
geonryu <ygun0626@gmail.com>
2021-12-08 00:25:45 +0900

    수정

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var swiper = new Swiper(".mySwiper", {
+    var swiper = new Swiper(".main-visual .mySwiper", {
         slidesPerView: "auto",
         spaceBetween: 30,
         freeMode: true,
@@ -15,10 +15,61 @@ $(document).ready(function () {
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
-        },
-        // breakpoints: {
-        //     1340: {
-        //     },
-        // },
+        }
     });
+
+    $(".btn-nav-opn").on("click", function() {
+        $(this).toggleClass("open");
+        let active = $(this).hasClass("open");
+
+        if(active) { 
+            $("nav").stop().fadeIn();
+            $("nav .gnb").addClass("active");
+        } else {
+            $("nav").stop().fadeOut();
+            $("nav .gnb").removeClass("active");
+        }
+    })
+
+    function scrollAnim(scrTo) {
+        $("html, body").stop().animate({
+            "scrollTop" : scrTo.offset().top
+        }, 650, "easeOutExpo");
+    }
+
+    $(".gnb li a").on("click", function(e) {
+        e.preventDefault();
+        const idx = $(this).parent().index();
+        $("nav").stop().fadeOut();
+        $("nav .gnb").removeClass("active");
+        $(".btn-nav-opn").removeClass("open");
+        if(idx===0){
+            scrollAnim($("section.about"))
+        } else if(idx===1) {
+            scrollAnim($("section.gallery"))
+        } else if(idx===2){
+            scrollAnim($("section.contact"))
+        }
+    });
+
+    var swiper = new Swiper(".gallery .mySwiper", {
+        slidesPerView: 1,
+        loop: true,
+        navigation: {
+            prevEl: ".gallery .swiper-button-prev",
+            nextEl: ".gallery .swiper-button-next",
+        },
+        autoplay: {
+            delay: 4500,
+            disableOnInteraction: false,
+        }
+    });
+
+    // 페이지 연결 준비중
+    $(".project-more a").on("click", function(e) {
+        e.preventDefault();
+        alert("페이지 준비중입니다.");
+    })
+
+    AOS.init();
 })
---
+++ js/map-api.js
@@ -0,0 +1,4 @@
+const appID = "5rkl30wkd1"
+
+var mapDiv = document.getElementById('map'); 
+var map = new naver.maps.Map(mapDiv);
