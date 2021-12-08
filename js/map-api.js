adf9b6224f424eb8675b93b31fe7a504029ac52c
geonryu <ygun0626@gmail.com>
2021-12-09 00:53:05 +0900

    naver map api prototype

--- js/map-api.js
+++ js/map-api.js
@@ -1,6 +1,11 @@
 var mapOptions = {
     center: new naver.maps.LatLng(35.1578510, 129.0498801),
-    zoom: 10
+    zoom: 50
 };
+
+var marker = new naver.maps.Marker({
+    center: new naver.maps.LatLng(35.1578510, 129.0498801),
+    map: map
+});
 
 var map = new naver.maps.Map('map', mapOptions);
