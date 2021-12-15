8bf530bfeee40c0dc36f20bec6cbd42b11f0781e
geonryu <ygun0626@gmail.com>
2021-12-15 23:10:24 +0900

    test2

--- js/map-api.js
+++ js/map-api.js
@@ -1,11 +1,30 @@
-// var mapOptions = {
+// // var mapOptions = {
+// //     center: new naver.maps.LatLng(129.048436, 35.1682553),
+// //     zoom: 15
+// // };
+
+
+// var mapOptions = new naver.maps.Marker({
 //     center: new naver.maps.LatLng(129.048436, 35.1682553),
 //     zoom: 15
-// };
+// });
+// var map = new naver.maps.Map('map', mapOptions);
 
+const locations = [{
+    place: "애드강츠비",
+    lat: 129.048436,
+    lng: 35.1682553
+}, ];
 
-var mapOptions = new naver.maps.Marker({
-    center: new naver.maps.LatLng(129.048436, 35.1682553),
-    zoom: 15
+var map = new naver.maps.Map('map', {
+    center: new naver.maps.LatLng(37.539922, 127.070609),
+    zoom: 14
 });
-var map = new naver.maps.Map('map', mapOptions);
+
+for (var i = 0; i < locations.length; i++) {
+    var marker = new naver.maps.Marker({
+        map: map,
+        title: locations[i].place,
+        position: new naver.maps.LatLng(locations[i].lat, locations[i].lng),
+    });
+}
