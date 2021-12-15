51627a9aac996f3c1c45f9c6fa423fd551a611a9
geonryu <ygun0626@gmail.com>
2021-12-15 23:37:23 +0900

    geo position edit

--- js/map-api.js
+++ js/map-api.js
@@ -12,19 +12,20 @@
 
 const locations = [{
     place: "애드강츠비",
-    lat: 129.048436,
-    lng: 35.1682553
+    lat: 35.1682419,
+    lng: 129.0484250
 }, ];
 
 var map = new naver.maps.Map('map', {
-    center: new naver.maps.LatLng(129.048436, 35.1682553),
-    zoom: 14
+    center: new naver.maps.LatLng(35.1682419, 129.0484250),
+    zoom: 15,
+    title: "애드강츠비"
 });
 
-// for (var i = 0; i < locations.length; i++) {
-//     var marker = new naver.maps.Marker({
-//         map: map,
-//         title: locations[i].place,
-//         position: new naver.maps.LatLng(locations[i].lat, locations[i].lng),
-//     });
-// }
+for (var i = 0; i < locations.length; i++) {
+    var marker = new naver.maps.Marker({
+        map: map,
+        title: locations[i].place,
+        position: new naver.maps.LatLng(locations[i].lat, locations[i].lng),
+    });
+}
