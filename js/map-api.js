0e79402d285ee3fb812b691ce0d98f2b75e182fb
geonryu <ygun0626@gmail.com>
2021-12-15 23:16:59 +0900

    test3

--- js/map-api.js
+++ js/map-api.js
@@ -17,14 +17,14 @@ const locations = [{
 }, ];
 
 var map = new naver.maps.Map('map', {
-    center: new naver.maps.LatLng(37.539922, 127.070609),
+    center: new naver.maps.LatLng(129.048436, 35.1682553),
     zoom: 14
 });
 
-for (var i = 0; i < locations.length; i++) {
-    var marker = new naver.maps.Marker({
-        map: map,
-        title: locations[i].place,
-        position: new naver.maps.LatLng(locations[i].lat, locations[i].lng),
-    });
-}
+// for (var i = 0; i < locations.length; i++) {
+//     var marker = new naver.maps.Marker({
+//         map: map,
+//         title: locations[i].place,
+//         position: new naver.maps.LatLng(locations[i].lat, locations[i].lng),
+//     });
+// }
